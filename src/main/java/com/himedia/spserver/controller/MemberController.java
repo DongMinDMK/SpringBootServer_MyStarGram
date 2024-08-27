5a550faf7b17e98a216a4b743595dc5eb1127070
DongMinDMK <syh6917@naver.com>
2024-08-27 14:53:24 +0900

    first commit

--- src/main/java/com/himedia/spserver/controller/MemberController.java
+++ src/main/java/com/himedia/spserver/controller/MemberController.java
@@ -56,7 +56,7 @@ public class MemberController {
 
     @GetMapping("/test")
     public String test(){
-        return "AWS SpringBoot Service Test1";
+        return "AWS SpringBoot Service Test2";
     }
 
     @GetMapping("/getLoginUser")
